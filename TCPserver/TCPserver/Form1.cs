b2d1ae55fb299eccc59adf8974e14a38bdd1a8c5
Святослав Волошко <voloshko.svyatoslav2@gmail.com>
2020-02-29 02:44:07 +0200

    TCP_Server

---
+++ TCPserver/TCPserver/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCPserver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Network myServer = new Network();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            myServer.RunServer();
+        }
+    }
+}
---
+++ TCPserver/TCPserver/Network.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPserver
+{
+    class Network
+    {
+        bool ServerState = true;
+        TcpClient client;
+        public string ByteConvert(byte[]Resive)
+        { int buf = Resive.Length;
+            string r = " ";
+            int f = 0;
+            for(int i = 0; i < buf; i++)
+            {
+                if (Resive[i]!=0)
+                {
+                    f++;
+                }
+            }
+            r = System.Text.Encoding.ASCII.GetString(Resive);
+            r = r.Substring(0, f);
+            return r;
+        }
+        public string SendRecive(string text,bool send){
+            string response = " ";
+            byte[] o = new byte[text.Length];
+            try
+            {
+                switch (send)
+                {
+                    case true:
+                        o = System.Text.Encoding.ASCII.GetBytes(text);
+                        client.Client.Send(o);
+                        break;
+                    case false:
+                        o = new byte[256];
+                        response = ByteConvert(o);
+                        break;
+                }
+
+            }
+            catch
+            {
+
+            }
+            return response;
+        }
+        void Listen()
+        {
+            IPAddress localAdress = IPAddress.Parse("0.0.0.0");
+            int port = 911;
+            TcpListener liss = new TcpListener(localAdress, port);
+            liss.Start();
+            while (ServerState)
+            {
+                client = liss.AcceptTcpClient();
+                SendRecive("Hello",true);
+                client.Client.Close();
+            }
+        }
+        public void RunServer()
+        {
+            ServerState = true;
+            Task S = new Task(Listen);
+            S.Start();
+        }
+    }
+}
